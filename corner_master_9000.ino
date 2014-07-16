25fd752c8cc302dbf576656cd92b3f2e748fede9
nickmyku <nickmyku@gmail.com>
2014-07-16 01:49:29 -0400

    half fixed the center fade function

--- corner_master_9000.ino
+++ corner_master_9000.ino
@@ -27,24 +27,28 @@ void loop() {
   //send data to neopixels
   //strip.show();
   
-  centerWipeFade(strip.Color(100, 0, 0), 22);
+  centerWipeFade(strip.Color(100, 0, 0), 35);
   delay(1500);
-  //clearDisplay();
-  //delay(1500);
+  clearDisplay();
+  delay(1500);
 }
 
 //fading wipe neopixel function, plays when microcontroller first starts
 void centerWipeFade(uint32_t c, uint8_t wait){
   //
   int red = 127;
-  int fade = 30;
+  int red_fade = 15;
   int tmp_red = red;
-  int fade_length = red/fade;
+  int fade_length = red/red_fade;
+  int loop_cycles = (strip.numPixels()/2)+fade_length+1;
+  int offset = strip.numPixels()/2;
   
   //primary fade loop
-  for(int i=0; i<(strip.numPixels()+fade_length+1); i++) {
-    for(int j=i; j>=0; j--) {
-      tmp_red = red-((i-j)*fade);
+  for(int i=0; i<loop_cycles; i++) {
+    //positive half
+    for(int j=(i+offset); j>=offset; j--) {
+      tmp_red = red-(((i+offset)-j)*red_fade);
+      //prevent a "negative" intensity from being written
       if(tmp_red < 0){
         tmp_red = 0;
       }
@@ -53,6 +57,19 @@ void centerWipeFade(uint32_t c, uint8_t wait){
         strip.setPixelColor(j, strip.Color(tmp_red, 0, 0));
       }
     }
+    //negative half
+    for(int k=offset-i; k<loop_cycles; k++) {
+      tmp_red = red-(k*red_fade);
+      //prevent a "negative" intensity from being written
+      if(tmp_red < 0){
+        tmp_red = 0;
+      }
+      //only write to the negative pixels
+      if(k<(strip.numPixels()/2)) {
+        strip.setPixelColor(k, strip.Color(tmp_red, 0, 0));
+      }
+     
+    } 
     strip.show();
     delay(wait);
   } 
