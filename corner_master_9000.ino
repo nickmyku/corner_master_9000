37caf8ec711df992eb6d2c8b624412cb487ccb07
nickmyku <nickmyku@gmail.com>
2014-07-18 20:36:50 -0400

    changed equation

--- corner_master_9000.ino
+++ corner_master_9000.ino
@@ -18,6 +18,8 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, DISPLAY_PIN, NEO_GRB + NEO_KHZ80
 void setup() {
   strip.begin();
   strip.show();  //turn pixels off
+  
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -31,6 +33,7 @@ void loop() {
   delay(1500);
   clearDisplay();
   delay(1500);
+  
 }
 
 //fading wipe neopixel function, plays when microcontroller first starts
@@ -40,11 +43,13 @@ void centerWipeFade(uint32_t c, uint8_t wait){
   int red_fade = 15;
   int tmp_red = red;
   int fade_length = red/red_fade;
-  int loop_cycles = (strip.numPixels()/2)+fade_length+1;
   int offset = strip.numPixels()/2;
+  int loop_cycles = offset+fade_length+1;
+  
   
   //primary fade loop
   for(int i=0; i<loop_cycles; i++) {
+    
     //positive half
     for(int j=(i+offset); j>=offset; j--) {
       tmp_red = red-(((i+offset)-j)*red_fade);
@@ -55,21 +60,27 @@ void centerWipeFade(uint32_t c, uint8_t wait){
       //only write to pixel if it physically exists
       if(j<strip.numPixels()){
         strip.setPixelColor(j, strip.Color(tmp_red, 0, 0));
+        //strip.setPixelColor(j-8, strip.Color(tmp_red, 0, 0));
       }
     }
+    
     //negative half
-    for(int k=offset-i; k<(loop_cycles+offset); k++) {
-      tmp_red = red-(k*red_fade);
+    for(int k=offset-i; k<loop_cycles; k++) {
+      Serial.print(k);
+      Serial.print(", ");
+      tmp_red = red-((k)*red_fade);
       //prevent a "negative" intensity from being written
       if(tmp_red < 0){
         tmp_red = 0;
       }
       //only write to the negative pixels
-      if(k<(strip.numPixels()/2)) {
+      if(k<offset) {
         strip.setPixelColor(k, strip.Color(tmp_red, 0, 0));
       }
      
-    } 
+    }
+    Serial.println(" ");
+    
     strip.show();
     delay(wait);
   } 
