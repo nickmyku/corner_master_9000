3a586c77b31097dc06251bcda8e6be57d10bd21f
nickmyku <nickmyku@gmail.com>
2014-07-19 20:59:29 -0400

    added fade in to zero value

--- corner_master_9000.ino
+++ corner_master_9000.ino
@@ -5,6 +5,8 @@
 #include <Adafruit_NeoPixel.h>
 
 #define DISPLAY_PIN 6
+#define HIGH_INTENSITY 127
+#define MAX_FORCE 1.5
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -15,9 +17,30 @@
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, DISPLAY_PIN, NEO_GRB + NEO_KHZ800);
 
+int offset = strip.numPixels()/2;
+
 void setup() {
+  //intitialize strip
   strip.begin();
-  strip.show();  //turn pixels off
+  //send all off to strip
+  strip.show();
+  
+  //start of fade sequence
+  centerWipeFade(HIGH_INTENSITY, 30);
+  
+  int intensity = 0;
+  int fade = 6;
+  //fade in zero force indicator
+  while(intensity < HIGH_INTENSITY) {
+    intensity = intensity + fade;
+    strip.setPixelColor(7, strip.Color(intensity, 0, 0));
+    strip.setPixelColor(8, strip.Color(intensity, 0, 0));
+    delay(50);
+    strip.show();
+  }
+    
+    
+  
   
   //Serial.begin(9600);
 }
@@ -29,21 +52,21 @@ void loop() {
   //send data to neopixels
   //strip.show();
   
-  centerWipeFade(strip.Color(100, 0, 0), 30);
-  delay(1500);
-  clearDisplay();
-  delay(1500);
+  //centerWipeFade(strip.Color(100, 0, 0), 30);
+  //delay(1500);
+  //clearDisplay();
+  //delay(1500);
   
 }
 
 //fading wipe neopixel function, plays when microcontroller first starts
-void centerWipeFade(uint32_t c, uint8_t wait){
+void centerWipeFade(uint8_t red, uint8_t wait){
   //
-  int red = 127;
+  
   int red_fade = 13;
   int tmp_red = red;
   int fade_length = red/red_fade;
-  int offset = strip.numPixels()/2;
+
   int loop_cycles = offset+fade_length+2;
   int addr = 0;
   
@@ -87,6 +110,14 @@ void centerWipeFade(uint32_t c, uint8_t wait){
     delay(wait);
   } 
   
+  
+  
+}
+
+//write to neopixel display
+void setForceDisplay(float force_value) {
+  //figure out value corresponding to a single pixel
+  float segment = MAX_FORCE/offset;
   
   
 }
